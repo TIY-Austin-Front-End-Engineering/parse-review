8f93a81b46f4d17a03c95748a39a419503f5843f
Leslie Sutton <l4sutton@gmail.com>
2015-10-12 15:47:37 -0500

    added the onaddchoice function to display the multiple choices when added

--- scripts/components/PostQuestionComponent.js
+++ scripts/components/PostQuestionComponent.js
@@ -1,23 +1,48 @@
 var React = require('react');
+var ReactDOM = require('')
 var Backbone = require('backbone');
 
 
 module.exports = React.createClass({
+	getInitialState: function(){
+		return (
+			{choices: []}
+		);
+	},
     render: function() {
-    	var questionChoices: [];
+    	var choiceRows = this.state.choices.map(function(choice){
+    		return(
+    			<label>
+    			<input type="radio" value={choice}/>
+    			{newChoice}
+    			</label>
+    		)
+    	});
         return (
             	<form>
             		<input type="text" ref="questionTitle" className="validate" />
-            		<input type="text" ref="questionContent" className="validate" />
-            		<input type="text" ref="choices" className="validate" />
-            		<input type="text" ref="choices" className="validate" />
-            		<input type="text" ref="choices" className="validate" />
-            		<input type="text" ref="choices" className="validate" />
+            		<input type="text" ref="choice" className="validate" />
+
+            		<button onClick = {this.onAddChoice}> Add Choice </button>
+            			{choiceRows}
             		<input type="text" ref="questionAnswer" className="validate" />
             		<button>Submit Question</button>
             	</form>
 
         );
     }
+    onSubmit: function(){
+
+    },
+    onAddChoice: function(){
+    	var newChoice = this.refs.choice.value;
+    	var currentChoices = this.state.choices;
+    	currentChoices.push(newChoice);
+    	this.setState({choices: currentChoices})
+    	
+	
+    	
+    }
 });
+
 
