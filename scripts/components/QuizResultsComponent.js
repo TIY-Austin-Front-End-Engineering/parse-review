7da663cedc0ebba4bd6b2ed287c3ebae57e7efe5
bob <bobmccarthyatx@gmail.com>
2015-10-12 16:19:52 -0500

    set up quiz results component, still need to test with data

--- scripts/components/QuizResultsComponent.js
+++ scripts/components/QuizResultsComponent.js
@@ -1,28 +1,74 @@
 var React = require('react');
 var QuizModel = require('../models/QuizModel');
-var query = new Parse.Query(QuizModel);
+var QuestionModel = require('../models/QuestionModel');
+var StudentAnswersModel = require('../models/StudentsAnswersModel');
+var quizQuery = new Parse.Query(QuizModel);
+var questionsQuery = new Parse.Query(QuestionModel);
+var studentAnswersQuery = new Parse.Query(StudentAnswersModel);
+var PossibleAnswersComponent = require('./components/PossibleAnswersComponent');
 
 module.exports = React.createClass({
 	getInitialState: function() {
 	    return {
+	    	user: this.props.user,
 	    	quiz: null,
+	    	questions: [],
 	        error: null
 	    }
 	},
 	componentWillMount: function() {
-		query
-		.find().then(
-			(quiz) => {
-				this.setState({quiz: quiz});
-			},
-			(err) => {
-				this.setState({error: err.message})
-			}
-		);
+
+		quizQuery.equalTo('objectId', this.props.quiz)
+		.first({
+			success: (result) => {
+        		this.setState({
+					quiz: result
+				});
+   			},
+    		error: (error) => {
+       	 		console.log('didnt find it');
+       	 		this.setState({
+					error: err.message
+				})
+    		}
+		});
+		questionsQuery.equalTo('quiz_id', this.props.quiz)
+		.find({
+			success: (result) => {
+				console.log(result);
+        		that.setState({
+					questions: result
+				});
+   			},
+    		error: (error) => {
+       	 		console.log('didnt find any questions');
+       	 		that.setState({
+					error: err.message
+				})
+    		}
+		});
+			
 	},
 	render: function() {
+		var questions = this.state.questions
+		.map(function(question) {
 		return (
 			<div>
+				<h2>{question.get('questionTitle')}</h2>
+				<PossibleAnswersComponent question={question.id} answers={question.get('questionChoices')} correctAnswer={question.get('correctChoice')} quizId={this.state.quiz}/>
+			</div>
+			);
+		})
+		return (
+			<div>
+				<div>
+					<div>Quiz Name: </div>
+					<div>User: </div>
+					<div>Percentage: </div>
+				</div>
+				<div>
+					{questions}
+				</div>
 			</div>
 		);
 	}
