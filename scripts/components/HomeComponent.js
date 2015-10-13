361b0bd243a4645a4a989d4056828f2a57f91351
Mike Mathew <m2mathew@me.com>
2015-10-13 12:37:43 -0500

    Converted spaces to tabs to match code standards

--- scripts/components/HomeComponent.js
+++ scripts/components/HomeComponent.js
@@ -10,14 +10,14 @@
 var React = require('react');
 
 module.exports = React.createClass({
-    render: function() {
-        return(
-            <div className="home-container">
-                <div className="home-title">
-                    <h1>Home</h1>
-                </div>
-                <img src="http://www.modred.co.uk/wp-content/uploads/2014/11/Illustrations-Owl-480x320.png"/>
-            </div>
-        )
-    }
+	render: function() {
+		return(
+			<div className="home-container">
+				<div className="home-title">
+					<h1>Home</h1>
+				</div>
+				<img src="http://www.modred.co.uk/wp-content/uploads/2014/11/Illustrations-Owl-480x320.png"/>
+			</div>
+		)
+	}
 });
