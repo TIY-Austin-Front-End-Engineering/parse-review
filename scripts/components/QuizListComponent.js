f287fa29801da330dbcccd37b6d9f51f44ef1b0e
Jmingus <jmingus@austin.rr.com>
2015-10-13 16:35:29 -0500

    added mobile responsive styles for quizList

--- scripts/components/QuizListComponent.js
+++ scripts/components/QuizListComponent.js
@@ -42,6 +42,8 @@ module.exports = React.createClass({
 		  )
 	},
 	fetch: function(){
+		this.query.descending("createdAt");
+		this.query.limit(5);
 		this.query.find().then(
 			(allQuizzes) => {
 				this.setState({quizList: allQuizzes})
