3938467ebf83fd4b40594c0d62b133cea237fda8
Mike Mathew <m2mathew@me.com>
2015-10-14 14:25:45 -0500

    Teacher can select a quiz

--- scripts/components/QuizDetailsComponent.js
+++ scripts/components/QuizDetailsComponent.js
@@ -5,21 +5,18 @@ var QuizModel = require('../models/QuizModel');
 var QuestionModel = require('../models/QuestionModel');
 var StudentAnswerModel = require('../models/StudentAnswerModel');
 
-
-
-module.exports = React.createClass({ 
+module.exports = React.createClass({
 	getInitialState: function() {
 	    return {
 	         currentQuestion:0,
 	    };
-	   
+
 	},
 	componentDidMount: function() {
 		this.start = new Date();
 		var query = new Parse.Query(QuizModel);
 		query.get(this.props.quizId).then(
 			(quiz) => {
-
 
 				var questionQuery = new Parse.Query(QuestionModel);
 				questionQuery.equalTo("quizId", quiz);
@@ -31,10 +28,8 @@ module.exports = React.createClass({
 						})
 					}
 				)
-
 			}
 		)
-
 	},
 	submitSolve: function(){
 		this.state.currentQuestion++;
@@ -53,36 +48,35 @@ module.exports = React.createClass({
 		this.setState({
 	         currentQuestion:this.state.currentQuestion;
 	    })
-	    
-	}, 
+	},
 	answerPicked: function(e){
-		this.currentQuestion.selectedChoiceId = e.currentTarget.value	
+		this.currentQuestion.selectedChoiceId = e.currentTarget.value
 	},
 	render: function() {
-		var questions = this.state.questions 
+		var questions = this.state.questions
 
 		if(questions == null){
-			return (<div>Loading Quiz...</div>); 
+			return (<div>Loading Quiz...</div>);
 		}
 
-		var questionVar = questions 
+		var questionVar = questions
 		var currentQuestion = questionVar[this.state.currentQuestion]
-		this.currentQuestion = currentQuestion 
+		this.currentQuestion = currentQuestion
 
-		var self = this;  
+		var self = this;
 		var choices = currentQuestion.get('questionChoices').map(function(qc){
 
 				return(<div><input value={qc} type='radio' name='radioAnswer' onChange={self.answerPicked} /> &nbsp;{qc}</div>);
-		}); 
-			
+		});
+
 			return (
 				<div className="Quiz">
 					<div>
 						{currentQuestion.get('questionContent')}
 					</div>
-					{choices} 
-					<button onClick={this.submitSolve}>Submit</button>				
-				</div>	
+					{choices}
+					<button onClick={this.submitSolve}>Submit</button>
+				</div>
 			)
 	}
 });
