2427f928ab7bf8a32eca4a0f5b9edf0443b18233
Noel Farris <noel.farris@gmail.com>
2015-10-15 13:59:05 -0500

    more bug fixes

--- scripts/components/CreateQuizComponent.js
+++ scripts/components/CreateQuizComponent.js
@@ -51,23 +51,14 @@ module.exports = React.createClass({
 
 	},
 	onSubmit: function(e){
-		//grabbing the name and id of new quiz and passing it through to edit quiz
-		if(this.refs.quizName.value === '' || this.refs.dateToStart.value === '' || this.refs.dateExpire.value === ''){
-				console.log('Please fill in all fields');
-				this.setState({errorMsg: 'Please fill in all fields'});
-		}else {
-			var newQuiz = new QuizModel({
-				quizTitle: this.refs.quizName.value,
-				startTime: new Date(this.refs.dateToStart.value),
-				expireTime: new Date(this.refs.dateExpire.value),
-				totalQuestions: 0
-			});
+		//grabbing the name and id of new quiz and passing it through to edit quiZ
 
 		e.preventDefault();
 		var newQuiz = new QuizModel({
 			quizTitle: this.refs.quizName.value,
 			startTime: new Date(this.refs.dateToStart.value),
-			expireTime: new Date(this.refs.dateToExpire.value)
+			expireTime: new Date(this.refs.dateToExpire.value),
+			totalQuestion: 0
 		});
 		console.log(this.refs.dateToStart.value);
 		if(!this.refs.dateToStart.value && !this.refs.dateToExpire.value)
@@ -104,5 +95,4 @@ module.exports = React.createClass({
 			});
 		}	
 	}
-}
 });
