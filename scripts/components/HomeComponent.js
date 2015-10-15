439b7cb908af53069efffc7db7be4d5d7acafa45
Jmingus <jmingus@austin.rr.com>
2015-10-15 11:51:48 -0500

    Added three images for homepage, working on styling of homepage, also fixed a global style issue in _post-question.scss

--- scripts/components/HomeComponent.js
+++ scripts/components/HomeComponent.js
@@ -27,13 +27,37 @@ module.exports = React.createClass({
 				</div>
 				<div className="row">
 					<div className="four columns">
-					column one
+						<div className="home-card">
+							<img src="../images/classAttendance.png" className="u-max-full-width" />
+							<div className="home-card-text">
+								<h3>Class Attendance</h3>
+								<hr />
+								<p>No longer will you have to keep track of class attendance!
+								 Our app does that for you! You can then spend the time that you saved making more quizzes!
+								 </p>
+							</div>
+						</div>
 					</div>
 					<div className="four columns">
-					columns two
+						<div className="home-card">
+							<img src="../images/analytics.png" className="u-max-full-width" />
+							<div className="home-card-text">
+								<h3>Content</h3>
+							</div>
+						</div>
 					</div>
 					<div className="four columns">
-					columns three
+						<div className="home-card">
+							<img src="../images/quizTaking.png" className="u-max-full-width" />
+							<div className="home-card-text">
+								<h3>Quiz Taking</h3>
+								<hr />
+								<p>Your students will be saying WOW! after they see how easy it is to take a quiz.
+								Iron Quizzes takes the hassle of setting up to take a quiz.
+								 Your students will have so much free time, maybe they could take more quizzes?
+								 </p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
