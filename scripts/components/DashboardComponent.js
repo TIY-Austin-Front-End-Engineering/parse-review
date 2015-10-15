bda9c39444f6dbb84dc4d7f50a7722fee738bec7
bob <bobmccarthyatx@gmail.com>
2015-10-15 12:04:33 -0500

    added create quiz link to dashboard, made entire div clickable, added border and spacing changes to dashboard.css

--- scripts/components/DashboardComponent.js
+++ scripts/components/DashboardComponent.js
@@ -8,20 +8,33 @@ module.exports = React.createClass({
 		return(
 			<div className="dashboard-container">
 				<div className="dashboard-title">
-					<h1><span className="analytics">Analytics</span> Dashboard</h1>
+					<h1>{Parse.User.current().get('username')+"'s"} Dashboard</h1>
 				</div>
-				<div className="dashboard-links">
-					<a href="#studentAnalytics"><img src="../../images/grades.png"/></a>
-					<h5>Student Averages</h5>
-				</div>
-				<div className="dashboard-links">
-					<a href="#classAnalytics"><img src="../../images/quiz.png"/></a>
-					<h5>Quiz Averages</h5>
-				</div>
-				<div className="dashboard-links">
-					<a href="#attendance"><img src="../../images/attendance.png"/></a>
-					<h5>Student Attendance</h5>
-				</div>
+				<a href="#createQuiz">
+					<div className="dashboard-links">
+						<img src="../../images/check7.png"/>
+						<h5>Create A Quiz</h5>
+					</div>
+				</a>
+				<a href="#studentAnalytics">
+					<div className="dashboard-links">
+						<img src="../../images/grades.png"/>
+						<h5>Student Averages</h5>
+					</div>
+				</a>
+				<a href="#classAnalytics">
+					<div className="dashboard-links">
+						<img src="../../images/quiz.png"/>
+						<h5>Quiz Averages</h5>
+					</div>
+				</a>
+				<a href="#attendance">
+					<div className="dashboard-links">
+						<img src="../../images/attendance.png"/>
+						<h5>Student Attendance</h5>
+					</div>
+				</a>
+				
 			</div>
 
 		)
