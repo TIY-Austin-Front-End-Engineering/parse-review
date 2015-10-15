c3368a8b0dced9d24bf6109941f5508296b4faf5
Whitney Murray <wnksmith92@yahoo.com>
2015-10-15 14:29:10 -0500

    changed name in regComp for styling

--- scripts/components/RegisterComponent.js
+++ scripts/components/RegisterComponent.js
@@ -30,7 +30,7 @@ module.exports= React.createClass ({
 							<div className="row">
 								<div className="twelve columns">
 									<label htmlFor="firstName">First Name</label>
-									<input className="u-full-width" ref="firstName" type="text" placeholder="Jill" id="firstName" />
+									<input className="u-full-width" ref="firstName" type="text" placeholder="Gill" id="firstName" />
 								</div>
 							</div>
 							<div className="row">
